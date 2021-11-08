5116da6341bd81cb46a2ac882f4ff782911446ef
amanchauhan71 <amanchauhan7172@gmail.com>
2021-11-08 22:37:33 +0530

    dvc done

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -3,9 +3,10 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml,create_directories
+from src.utils.common import read_yaml, create_directories
 from src.utils.data_mgmt import process_posts
 import random
+
 
 STAGE = "One"
 
@@ -16,32 +17,33 @@ logging.basicConfig(
     filemode="a"
     )
 
-def main(config_path,params_path):
-
-    ## Converting XML data to tsv
+def main(config_path, params_path):
+    ## converting XML data tsv
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
     source_data = config["source_data"]
-    input_data = os.path.join(source_data["data_dir"],source_data["data_file"])
+    input_data = os.path.join(source_data["data_dir"], source_data["data_file"])
 
     split = params["prepare"]["split"]
     seed = params["prepare"]["seed"]
+
     random.seed(seed)
 
     artifacts = config["artifacts"]
-
-    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
     create_directories([prepared_data_dir_path])
 
-    train_data_path = os.path.join(prepared_data_dir_path,artifacts["TRAIN_DATA"])
-    test_data_path = os.path.join(prepared_data_dir_path,artifacts["TEST_DATA"])
-
+    train_data_path = os.path.join(prepared_data_dir_path, artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepared_data_dir_path, artifacts["TEST_DATA"])
+    
     encode = "utf8"
     with open(input_data, encoding=encode) as fd_in:
         with open(train_data_path, "w", encoding=encode) as fd_out_train:
             with open(test_data_path, "w", encoding=encode) as fd_out_test:
                 process_posts(fd_in, fd_out_train, fd_out_test, "<python>", split)
+
+
 
 
 if __name__ == '__main__':
@@ -53,8 +55,8 @@ if __name__ == '__main__':
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config,params_path=parsed_args.params)
-        logging.info(f">>>>> stage {STAGE} completed! all the data are saved in local <<<<</n")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
         raise e
